029db44f50c9552c4ef8e2d74a7e4bf755072e22
J-dar Siegfred Rodriguez <siegfred.rodriguez@gmail.com>
2021-03-27 16:53:29 +0800

    clean up

--- src/main/java/com/codegallery/librarystatistics/controller/LibraryStatController.java
+++ src/main/java/com/codegallery/librarystatistics/controller/LibraryStatController.java
@@ -1,9 +1,9 @@
 package com.codegallery.librarystatistics.controller;
 
 import com.codegallery.librarystatistics.domain.model.BorrowerStats;
-import com.codegallery.librarystatistics.domain.repository.BorrowerRepository;
 import com.codegallery.librarystatistics.domain.service.LibraryStatisticService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -11,15 +11,14 @@ import java.util.UUID;
 @RestController
 public class LibraryStatController {
 
-    private BorrowerRepository borrowerRepository;
     private LibraryStatisticService libraryStatisticService;
 
     public LibraryStatController(LibraryStatisticService libraryStatisticService) {
         this.libraryStatisticService = libraryStatisticService;
     }
 
-    @GetMapping("/stats")
-    public BorrowerStats getBooks() {
-        return libraryStatisticService.getBorrowerStats(UUID.fromString("3b8e929e-ad56-431f-9d3f-1ea6252cd5fc"));
+    @GetMapping("/stats/{borrowerId}")
+    public BorrowerStats getBooks(@PathVariable String borrowerId) {
+        return libraryStatisticService.getBorrowerStats(UUID.fromString(borrowerId));
     }
 }
